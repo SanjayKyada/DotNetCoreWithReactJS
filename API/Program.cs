24c22cafc8c735e138e4429b1eabe9c27661565a
SanjayKyada <53945338+SanjayKyada@users.noreply.github.com>
2023-01-23 12:44:43 +0000

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,9 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt=>
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConn"));
+}
+);
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicyRules", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+
+    });
 }
 );
 var app = builder.Build();
@@ -22,24 +31,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("CorsPolicyRules");
 app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope=app.Services.CreateScope();
-var services=scope.ServiceProvider;
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
 
 try
 {
-    var context= services.GetRequiredService<DataContext>();
+    var context = services.GetRequiredService<DataContext>();
     await context.Database.MigrateAsync();
     await Seed.SeedData(context);
 }
 catch (Exception ex)
 {
-    var logger=services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"Error occured in PRogram class");
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Error occured in PRogram class");
 }
 
 
--- client-uiapp/src/App.tsx
+++ client-uiapp/src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import ListItem from 'semantic-ui-react/dist/commonjs/elements/List/ListItem';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useState(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        console.log(response);
+        setActivities(response.data)
+      })
+  }
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header as='h2' icon={'users'} content='Jadiyo User'/>
+        <List>
+        {
+          activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.titile}
+            </List.Item>
+          )
+          )
+        }
+        </List>
+        <Button content='Test Button'></Button>
+        <h1>Hi.. Try Again</h1>
+        {/* {
+          activities.map((activity: any) =>
+          (
+            <p>{activity.venue} </p>
+          )
+          )
+        } */}
     </div>
   );
 }
